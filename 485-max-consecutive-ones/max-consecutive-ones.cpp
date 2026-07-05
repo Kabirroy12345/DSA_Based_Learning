c53ef4a8c19a4d225a9a5d4ab7866b92026b1cd3
Kabir Roy <144699626+Kabirroy12345@users.noreply.github.com>
2026-07-05 15:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (33.39%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -0,0 +1,40 @@
+// class Solution {
+// public:
+//     int findMaxConsecutiveOnes(vector<int>& arr) {
+//         int totalOnes = 0;
+//         int maxOnes = 0;
+
+//         for(int i = 0; i < arr.size(); i++) {
+//             if(arr[i] == 1) {
+//                 totalOnes += 1;
+//             }
+//             if(arr[i] == 0) {
+//                 if(totalOnes > maxOnes) {
+//                     maxOnes = totalOnes;
+//                 }
+//                 totalOnes = 0;
+//             }
+//         }
+
+//         return max(maxOnes, totalOnes);
+//     }
+// };
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& arr) {
+        int n=arr.size();
+        int count=0;
+        int max_count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==1){
+                count++;
+                max_count=max(max_count,count);
+
+            }
+            else{
+                count=0;
+            }
+        }
+        return max_count;
+    }
+};
