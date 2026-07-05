f3d6f30164d9790a39158764850cd782575a255b
Kabir Roy <144699626+Kabirroy12345@users.noreply.github.com>
2026-07-05 14:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (80.68%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& num) {
-        int n=num.size();
-        int s=0;
-        int e=n-1;
-        while(s<e){
-            swap(num[s++],num[e--]);
+        int n=num.size();  //total size
+        int s=0;   //start pointer
+        int e=n-1;  //end pointer
+        while(s<e){    //until they cross
+            swap(num[s],num[e]);  //swap and increment
+            s++;
+            e--;
         }
     }
 };
